216de278a2fdb9d2a42e4aceb940a580152b6c90
Thalisu <thalisondmcc@gmail.com>
2025-01-15 17:14:30 -0300

    feat: add initial migration for Cloth and Description models with relationships

---
+++ Migrations/20250115154604_init.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dress_u_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cloth",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Discount = table.Column<float>(type: "float(1)", nullable: false),
+                    Images = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cloth", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Description",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ClothId = table.Column<int>(type: "int", nullable: false),
+                    About = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tecnical = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Description", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Description_Cloth_ClothId",
+                        column: x => x.ClothId,
+                        principalTable: "Cloth",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Description_ClothId",
+                table: "Description",
+                column: "ClothId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Description");
+
+            migrationBuilder.DropTable(
+                name: "Cloth");
+        }
+    }
+}

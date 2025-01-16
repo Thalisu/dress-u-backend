117a119648e4e5cbea5b46852af9f58647185afd
Thalisu <thalisondmcc@gmail.com>
2025-01-16 17:38:08 -0300

    feat: update ProductController to use async methods for database operations

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -6,6 +6,7 @@ using dress_u_backend.data;
 using dress_u_backend.Dtos.Cloth;
 using dress_u_backend.Mappers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace dress_u_backend.Controllers
 {
@@ -20,16 +21,17 @@ namespace dress_u_backend.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll()
+        public async Task<IActionResult> GetAll()
         {
-            var products = _context.Cloths.ToList().Select(c => c.ToClothDto());
-            return Ok(products);
+            var cloths = await _context.Cloths.ToListAsync();
+            var clothDto = cloths.Select(c => c.ToClothDto());
+            return Ok(clothDto);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetById([FromRoute] int id)
+        public async Task<IActionResult> GetById([FromRoute] int id)
         {
-            var product = _context.Cloths.Find(id);
+            var product = await _context.Cloths.FindAsync(id);
             if (product == null)
             {
                 return NotFound();
@@ -38,17 +40,17 @@ namespace dress_u_backend.Controllers
             return Ok(product.ToClothDto());
         }
         [HttpPost]
-        public IActionResult Create([FromBody] CreateClothRequestDto clothDto)
+        public async Task<IActionResult> Create([FromBody] CreateClothRequestDto clothDto)
         {
             var cloth = clothDto.ToClothFromCreateDto();
-            _context.Cloths.Add(cloth);
-            _context.SaveChanges();
+            await _context.Cloths.AddAsync(cloth);
+            await _context.SaveChangesAsync();
             return CreatedAtAction(nameof(GetById), new { id = cloth.Id }, cloth.ToClothDto());
         }
         [HttpPut("{id}")]
-        public IActionResult Update([FromRoute] int id, [FromBody] UpdateClothRequestDto clothDto)
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateClothRequestDto clothDto)
         {
-            var cloth = _context.Cloths.Find(id);
+            var cloth = await _context.Cloths.FindAsync(id);
             if (cloth == null)
             {
                 return NotFound();
@@ -60,21 +62,21 @@ namespace dress_u_backend.Controllers
             cloth.Images = clothDto.Images;
             cloth.Categories = clothDto.Categories;
             cloth.Description = clothDto.Description;
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return Ok(cloth.ToClothDto());
         }
         [HttpDelete("{id}")]
-        public IActionResult Delete([FromRoute] int id)
+        public async Task<IActionResult> Delete([FromRoute] int id)
         {
-            var cloth = _context.Cloths.Find(id);
+            var cloth = await _context.Cloths.FirstOrDefaultAsync(x => x.Id == id);
             if (cloth == null)
             {
                 return NotFound();
             }
 
             _context.Cloths.Remove(cloth);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return NoContent();
         }

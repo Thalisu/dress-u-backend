f406af9ee58975d9fef2322696ea4475198cd9ed
Thalisu <thalisondmcc@gmail.com>
2025-01-16 17:19:59 -0300

    feat: add UpdateClothRequestDto for updating existing cloth items

---
+++ Dtos/Cloth/UpdateClothRequestDto.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dress_u_backend.models;
+
+namespace dress_u_backend.Dtos.Cloth
+{
+    public class UpdateClothRequestDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public float Discount { get; set; }
+        public string[] Images { get; set; } = [];
+        public Description? Description { get; set; }
+        public ICollection<Category> Categories { get; set; } = [];
+    }
+}

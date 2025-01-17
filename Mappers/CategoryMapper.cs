277525f9fd5cf1636d325885beb87ca7182cedb9
Thalisu <thalisondmcc@gmail.com>
2025-01-16 23:37:37 -0300

    feat: add CategoryMapper for converting between Category and DTOs

---
+++ Mappers/CategoryMapper.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dress_u_backend.Dtos.Category;
+using dress_u_backend.models;
+
+namespace dress_u_backend.Mappers
+{
+    public static class CategoryMapper
+    {
+        public static CategoryDto ToCategoryDto(this Category category)
+        {
+            return new CategoryDto
+            {
+                Id = category.Id,
+                Type = category.Type,
+                Cloths = category.Cloths,
+            };
+        }
+
+        public static Category ToCategoryFromCreateDto(this CreateCategoryRequestDto categoryDto)
+        {
+            return new Category
+            {
+                Type = categoryDto.Type,
+            };
+        }
+        public static Category ToCategoryFromUpdateDto(this UpdateCategoryRequestDto categoryDto)
+        {
+            return new Category
+            {
+                Type = categoryDto.Type,
+                Cloths = categoryDto.Cloths,
+            };
+        }
+    }
+}

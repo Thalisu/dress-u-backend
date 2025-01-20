84deae244a84e3f5b94f0a618b8f5791d3b8d386
Thalisu <thalisondmcc@gmail.com>
2025-01-20 11:37:06 -0300

    feat: remove required attributes from Title, Price, Images, and Categories in UpdateClothRequestDto

--- Dtos/Cloth/UpdateClothRequestDto.cs
+++ Dtos/Cloth/UpdateClothRequestDto.cs
@@ -9,20 +9,16 @@ namespace dress_u_backend.Dtos.Cloth
 {
     public class UpdateClothRequestDto
     {
-        [Required]
         [MinLength(3, ErrorMessage = "Title must be at least 3 characters long")]
         [MaxLength(50, ErrorMessage = "Title cannot exceed 50 characters")]
         public string Title { get; set; } = string.Empty;
-        [Required]
         [Range(0, 1000000, ErrorMessage = "Price must be between 0 and 1000000")]
         public decimal Price { get; set; }
         [Range(0, 1, ErrorMessage = "Discount must be between 0 and 1")]
         public float Discount { get; set; }
-        [Required]
         [MinLength(1, ErrorMessage = "At least one image is required")]
         public string[] Images { get; set; } = [];
         /*         public Models.Description? Description { get; set; } */
-        [Required]
         [MinLength(1, ErrorMessage = "At least one category is required")]
         public List<UseCategoryDto> Categories { get; set; } = [];
     }

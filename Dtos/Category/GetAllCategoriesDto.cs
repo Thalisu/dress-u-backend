881493e21263564af178cf50a61e41e2bdd15846
Thalisu <thalisondmcc@gmail.com>
2025-01-20 11:39:17 -0300

    feat: add GetAllCategoriesDto for category retrieval

---
+++ Dtos/Category/GetAllCategoriesDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dress_u_backend.Dtos.Cloth;
+
+namespace dress_u_backend.Dtos.Category
+{
+    public class GetAllCategoriesDto
+    {
+        public int Id { get; set; }
+        public string Type { get; set; } = "";
+    }
+}

6604b4a9651d2d4b7db6b7b3ab27997a42a872a7
Thalisu <thalisondmcc@gmail.com>
2025-01-19 22:32:16 -0300

    feat: update Categories mapping in ClothMapper to use CategoryMapper for DTO conversion

--- Mappers/ClothMapper.cs
+++ Mappers/ClothMapper.cs
@@ -32,7 +32,7 @@ namespace dress_u_backend.Mappers
                 Price = clothDto.Price,
                 Discount = clothDto.Discount,
                 Images = clothDto.Images,
-                /*                 Categories = clothDto.Categories, */
+                Categories = [.. clothDto.Categories.Select(CategoryMapper.ToCategoryFromDto)],
                 /*                 Description = clothDto.Description, */
             };
         }

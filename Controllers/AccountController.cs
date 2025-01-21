d7f4369000c9dbb72de224edb93a756359e05be7
Thalisu <thalisondmcc@gmail.com>
2025-01-21 09:50:14 -0300

    feat: add login endpoint to AccountController for user authentication

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -8,15 +8,17 @@ using dress_u_backend.Mappers;
 using dress_u_backend.models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace dress_u_backend.Controllers
 {
     [ApiController]
     [Route("/account")]
-    public class AccountController(UserManager<AppUser> userManager, ITokenService tokenService) : ControllerBase
+    public class AccountController(UserManager<AppUser> userManager, ITokenService tokenService, SignInManager<AppUser> signInManager) : ControllerBase
     {
         private readonly UserManager<AppUser> _userManager = userManager;
         private readonly ITokenService _tokenService = tokenService;
+        private readonly SignInManager<AppUser> _signInManager = signInManager;
         [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterDto registerDto)
         {
@@ -49,8 +51,36 @@ namespace dress_u_backend.Controllers
                 return StatusCode(500, e);
             }
         }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var hasEmail = !string.IsNullOrEmpty(loginDto.Email);
+            var hasUser = !string.IsNullOrEmpty(loginDto.Username);
+            if (!hasEmail && !hasUser)
+            {
+                var msg = "Email or Username is required.";
+                ModelState.AddModelError("Email", msg);
+                ModelState.AddModelError("Username", msg);
+            }
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
 
+            var user = hasEmail
+            ? await _userManager.Users.FirstOrDefaultAsync(u => u.Email == loginDto.Email)
+            : await _userManager.Users.FirstOrDefaultAsync(u => u.UserName == loginDto.Username);
 
+            if (user == null)
+            {
+                return Unauthorized("User not found.");
+            }
 
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+            if (!result.Succeeded)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(user.ToUserDto(_tokenService.CreateToken(user)));
+        }
     }
 }

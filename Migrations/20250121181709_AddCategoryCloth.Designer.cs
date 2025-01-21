0a3d69d12ee31d3f3d4859569ff8667ef184d1db
Thalisu <thalisondmcc@gmail.com>
2025-01-21 15:29:06 -0300

    feat: add migration for CategoryCloth many-to-many relationship and update model snapshot

--- Migrations/20250121181709_AddCategoryCloth.Designer.cs
+++ Migrations/20250121181709_AddCategoryCloth.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dress_u_backend.data;
 
@@ -11,9 +12,11 @@ using dress_u_backend.data;
 namespace dress_u_backend.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250121181709_AddCategoryCloth")]
+    partial class AddCategoryCloth
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250121181709_AddCategoryCloth.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dress_u_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoryCloth : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryCloth");
+
+            migrationBuilder.CreateTable(
+                name: "CategoryCloths",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    ClothId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryCloths", x => new { x.ClothId, x.CategoryId });
+                    table.ForeignKey(
+                        name: "FK_CategoryCloths_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoryCloths_Cloths_ClothId",
+                        column: x => x.ClothId,
+                        principalTable: "Cloths",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryCloths_CategoryId",
+                table: "CategoryCloths",
+                column: "CategoryId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CategoryCloths");
+
+            migrationBuilder.CreateTable(
+                name: "CategoryCloth",
+                columns: table => new
+                {
+                    CategoriesId = table.Column<int>(type: "int", nullable: false),
+                    ClothsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryCloth", x => new { x.CategoriesId, x.ClothsId });
+                    table.ForeignKey(
+                        name: "FK_CategoryCloth_Categories_CategoriesId",
+                        column: x => x.CategoriesId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoryCloth_Cloths_ClothsId",
+                        column: x => x.ClothsId,
+                        principalTable: "Cloths",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryCloth_ClothsId",
+                table: "CategoryCloth",
+                column: "ClothsId");
+        }
+    }
+}

587aa7f13b4fce4587cac9e115785c7e898f4131
Thalisu <thalisondmcc@gmail.com>
2025-01-21 15:29:21 -0300

    feat: add CategoryCloth DbSet and configure many-to-many relationship in ApplicationDBContext

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using dress_u_backend.models;
+using dress_u_backend.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -19,9 +20,21 @@ namespace dress_u_backend.data
         public DbSet<Cloth> Cloths { get; set; }
         public DbSet<Description> Description { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<CategoryCloth> CategoryCloths { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<CategoryCloth>()
+                .HasOne(cc => cc.Cloth)
+                .WithMany(c => c.CategoryCloths)
+                .HasForeignKey(cc => cc.ClothId);
+            modelBuilder.Entity<CategoryCloth>()
+                .HasOne(cc => cc.Category)
+                .WithMany(c => c.CategoryCloths)
+                .HasForeignKey(cc => cc.CategoryId);
+
+            modelBuilder.Entity<CategoryCloth>().HasKey(cc => new { cc.ClothId, cc.CategoryId });
+
             List<IdentityRole> roles = [
                 new(){
                     Id = "1",

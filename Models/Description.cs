9678d51f852c112b9a76e1a4d412bcb89c0bed0e
Thalisu <thalisondmcc@gmail.com>
2025-01-22 18:40:24 -0300

    Revert "fix: update Cloth and Description models to handle nullable Cloth reference and add DescriptionId for improved data relationships"
    This reverts commit f192a8e2faa52d861ddbfd187b81a8a5632b64d3.

--- Models/Description.cs
+++ Models/Description.cs
@@ -9,7 +9,7 @@ namespace dress_u_backend.models
     {
         public int Id { get; set; }
         public int ClothId { get; set; }
-        public Cloth? Cloth { get; set; }
+        public Cloth Cloth { get; set; } = null!;
         public string About { get; set; } = "";
         public string Tecnical { get; set; } = "";
     }

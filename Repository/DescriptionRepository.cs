24d7bae8fd7ddbebfe81607379d8e138ecbc218d
Thalisu <thalisondmcc@gmail.com>
2025-01-24 14:15:07 -0300

    feat: update CreateAsync and add UpdateAsync methods in DescriptionRepository with optional save parameter

--- Repository/DescriptionRepository.cs
+++ Repository/DescriptionRepository.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using dress_u_backend.data;
+using dress_u_backend.Dtos.Description;
 using dress_u_backend.Interfaces;
+using dress_u_backend.Mappers;
 using dress_u_backend.models;
 
 namespace dress_u_backend.Repository
@@ -12,11 +14,30 @@ namespace dress_u_backend.Repository
     {
         private readonly ApplicationDBContext _context = context;
 
-        public async Task<Description> CreateAsync(Description description)
+        public async Task<Description> CreateAsync(Description description, bool save = true)
         {
             await _context.Description.AddAsync(description);
-            await _context.SaveChangesAsync();
+            if (save) await _context.SaveChangesAsync();
             return description;
+        }
+        public async Task<DescriptionDto?> UpdateAsync(int clothId, UpdateDescriptionRequestDto descriptionDto, bool save = true)
+        {
+
+            var descriptionModel = descriptionDto.ToDescriptionFromUpdateDto(clothId);
+            var existingDescription = _context.Description.FirstOrDefault(d => d.ClothId == clothId);
+
+            if (existingDescription == null)
+            {
+                var description = await CreateAsync(descriptionModel);
+                return description.ToDescriptionDto();
+            }
+
+            existingDescription.About = descriptionModel.About;
+            existingDescription.Tecnical = descriptionModel.Tecnical;
+            if (save) await _context.SaveChangesAsync();
+
+            return existingDescription.ToDescriptionDto();
+
         }
     }
 }

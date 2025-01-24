ebadcc104cd1d733eeecd4578be53df9ad94bd66
Thalisu <thalisondmcc@gmail.com>
2025-01-24 14:14:38 -0300

    feat: add mappers for creating and updating CategoryCloth DTOs from Cloth and category IDs

--- Mappers/CategoryClothMapper.cs
+++ Mappers/CategoryClothMapper.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dress_u_backend.Dtos.CategoryCloth;
 using dress_u_backend.models;
 using dress_u_backend.Models;
 
@@ -9,13 +10,60 @@ namespace dress_u_backend.Mappers
 {
     public static class CategoryClothMapper
     {
-        public static CategoryCloth ToCategoryCloth(int categoryId, int clothId)
+        public static CreateCategoryClothRequestDto ToCreateCategoryClothDtoFromCategoryIds(this Cloth cloth, List<int> categoryIds)
+        {
+            return new CreateCategoryClothRequestDto
+            {
+                ClothId = cloth.Id,
+                CategoryIds = categoryIds
+            };
+        }
+        public static UpdateCategoryClothRequestDto ToUpdateCategoryClothDtoFromCategoryIds(this Cloth cloth, List<int> categoryIds)
+        {
+            return new UpdateCategoryClothRequestDto
+            {
+                ClothId = cloth.Id,
+                CategoryIds = categoryIds
+            };
+        }
+        public static List<CategoryCloth> ToCategoryClothFromCreateDto(this CreateCategoryClothRequestDto categoryClothDto)
+        {
+            return [.. categoryClothDto.CategoryIds.Select(
+                    cId => new CategoryCloth
+                        { CategoryId = cId, ClothId = categoryClothDto.ClothId}
+                  )];
+        }
+        public static List<CategoryCloth> ToCategoryClothFromUpdateDto(this UpdateCategoryClothRequestDto categoryClothDto)
+        {
+            return [.. categoryClothDto.CategoryIds.Select(
+                    cId => new CategoryCloth
+                        { CategoryId = cId, ClothId = categoryClothDto.ClothId}
+                  )];
+        }
+
+        public static CategoryCloth ToCategoryClothFromCloth(this Cloth cloth, int categoryId)
         {
             return new CategoryCloth
             {
                 CategoryId = categoryId,
+                ClothId = cloth.Id
+            };
+        }
+        public static CategoryCloth ToCategoryClothFromCategory(this Category category, int clothId)
+        {
+            return new CategoryCloth
+            {
+                CategoryId = category.Id,
                 ClothId = clothId
             };
         }
+        /*         public static CategoryCloth ToCategoryClothFromUpdateDto(this UpdateCategoryClothRequestDto categoryClothRequestDto)
+                {
+                    return new CategoryCloth
+                    {
+                        CategoryId = categoryClothRequestDto.CategoryId,
+                        ClothId = categoryClothRequestDto.ClothId
+                    };
+                } */
     }
 }

690c5e656f9962140bb565094f14afb46ac057d6
Thalisu <thalisondmcc@gmail.com>
2025-01-24 14:14:31 -0300

    feat: update IDescriptionRepository to include optional save parameter and add UpdateAsync method for description updates

--- Interfaces/IDescriptionRepository.cs
+++ Interfaces/IDescriptionRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dress_u_backend.Dtos.Description;
 using dress_u_backend.models;
 using Microsoft.OpenApi.Any;
 
@@ -9,6 +10,7 @@ namespace dress_u_backend.Interfaces
 {
     public interface IDescriptionRepository
     {
-        Task<Description> CreateAsync(Description description);
+        Task<Description> CreateAsync(Description description, bool save = true);
+        Task<DescriptionDto?> UpdateAsync(int clothId, UpdateDescriptionRequestDto descriptionDto, bool save = true);
     }
 }

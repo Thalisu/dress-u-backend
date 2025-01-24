ed39bee175ff67dd7b007fe1b80f1b7ccf2bfec4
Thalisu <thalisondmcc@gmail.com>
2025-01-24 14:14:23 -0300

    feat: update ICategoryClothRepository to use DTOs for category cloth creation and updates

--- Interfaces/ICategoryClothRepository.cs
+++ Interfaces/ICategoryClothRepository.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using dress_u_backend.Dtos.Category;
-using dress_u_backend.models;
+using dress_u_backend.Dtos.CategoryCloth;
+using dress_u_backend.Dtos.Description;
 using dress_u_backend.Models;
 
 namespace dress_u_backend.Interfaces
@@ -11,6 +12,7 @@ namespace dress_u_backend.Interfaces
     public interface ICategoryClothRepository
     {
         Task<List<CategoryOnlyDto?>> GetClothCategoriesByClothId(int clothId);
-        Task<List<CategoryCloth>> CreateAsync(List<CategoryCloth> categoryCloths);
+        Task<CreateCategoryClothRequestDto?> CreateAsync(CreateCategoryClothRequestDto categoryClothDto, bool save = true);
+        Task<UpdateCategoryClothRequestDto?> UpdateAsync(UpdateCategoryClothRequestDto categoryClothDto, bool save = true);
     }
 }

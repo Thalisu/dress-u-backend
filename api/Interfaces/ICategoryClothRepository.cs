a1b0fce2cadd70d59a8e3b3ca6b98168809c5963
Thalisu <thalisondmcc@gmail.com>
2025-01-29 22:17:45 -0300

    feat: update ICategoryClothRepository and CategoryClothRepository to use Result types for improved error handling

--- api/Interfaces/ICategoryClothRepository.cs
+++ api/Interfaces/ICategoryClothRepository.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dress_u_backend.Common;
 using dress_u_backend.Dtos.Category;
 using dress_u_backend.Dtos.CategoryCloth;
 using dress_u_backend.Dtos.Description;
@@ -11,8 +12,10 @@ namespace dress_u_backend.Interfaces
 {
     public interface ICategoryClothRepository
     {
-        Task<List<CategoryOnlyDto?>> GetClothCategoriesByClothId(int clothId);
-        Task<CreateCategoryClothRequestDto?> CreateAsync(CreateCategoryClothRequestDto categoryClothDto, bool save = true);
-        Task<UpdateCategoryClothRequestDto?> UpdateAsync(UpdateCategoryClothRequestDto categoryClothDto, bool save = true);
+        Task<Result<CategoriesDto>> GetByClothId(int clothId);
+        Task<Result<CreateCategoryClothRequestDto>> CreateAsync(
+            CreateCategoryClothRequestDto categoryClothDto, bool save = true);
+        Task<Result<UpdateCategoryClothRequestDto>> UpdateAsync(
+            UpdateCategoryClothRequestDto categoryClothDto, bool save = true);
     }
 }
--- api/Repository/CategoryClothRepository.cs
+++ api/Repository/CategoryClothRepository.cs
@@ -2,42 +2,66 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using dress_u_backend.Common;
+using dress_u_backend.Common.Errors;
 using dress_u_backend.Data;
 using dress_u_backend.Dtos.Category;
 using dress_u_backend.Dtos.CategoryCloth;
 using dress_u_backend.Interfaces;
 using dress_u_backend.Mappers;
 using dress_u_backend.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 namespace dress_u_backend.Repository
 {
-    public class CategoryClothRepository(ApplicationDBContext context) : ICategoryClothRepository
+    public class CategoryClothRepository(ApplicationDBContext context)
+        : ICategoryClothRepository
     {
         private readonly ApplicationDBContext _context = context;
 
-        public async Task<List<CategoryOnlyDto?>> GetClothCategoriesByClothId(int clothId)
+        public async Task<Result<CategoriesDto>> GetByClothId(int clothId)
         {
-            return await _context.CategoryCloths.Where(cc => cc.ClothId == clothId)
-                .Select(cc => cc.Category != null ? cc.Category.ToCategoryDto() : null)
-                .ToListAsync();
+            var categories = new CategoriesDto
+            {
+                Categories = await _context.CategoryCloths
+                .Where(cc => cc.ClothId == clothId)
+                .Select(cc => cc.Category.ToCategoryDto()).ToListAsync()
+            };
+
+            if (!categories.Categories.Any())
+            {
+                return ApiErrors.NotFound("Categories");
+            }
+
+            return categories;
         }
-        public async Task<CreateCategoryClothRequestDto?> CreateAsync(CreateCategoryClothRequestDto categoryClothDto, bool save = true)
+        public async Task<Result<CreateCategoryClothRequestDto>> CreateAsync(
+            CreateCategoryClothRequestDto categoryClothDto, bool save = true)
         {
             var categoryCloths = categoryClothDto.ToCategoryClothFromCreateDto();
             await _context.CategoryCloths.AddRangeAsync(categoryCloths);
+
             if (save) await _context.SaveChangesAsync();
+
             return categoryClothDto;
         }
 
 
-        public async Task<UpdateCategoryClothRequestDto?> UpdateAsync(UpdateCategoryClothRequestDto categoryClothDto, bool save = true)
+        public async Task<Result<UpdateCategoryClothRequestDto>> UpdateAsync(
+            UpdateCategoryClothRequestDto categoryClothDto, bool save = true)
         {
-            var CategoryClothModels = categoryClothDto.ToCategoryClothFromUpdateDto();
-            var ExistingCategories = await _context.CategoryCloths.Where(cc => cc.ClothId == categoryClothDto.ClothId).ToListAsync();
+            var CategoryClothModels = categoryClothDto
+                .ToCategoryClothFromUpdateDto();
+
+            var ExistingCategories = await _context.CategoryCloths.Where(
+                cc => cc.ClothId == categoryClothDto.ClothId).ToListAsync();
+
             _context.CategoryCloths.RemoveRange(ExistingCategories);
             await _context.CategoryCloths.AddRangeAsync(CategoryClothModels);
+
             if (save) await _context.SaveChangesAsync();
+
             return categoryClothDto;
         }
     }

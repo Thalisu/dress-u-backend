e40d9be352e8729f6522c41980ef9c886b20412a
Thalisu <thalisondmcc@gmail.com>
2025-01-29 23:31:53 -0300

    feat: add Stock column to Cloths table and update CategoryCloth model definition

--- api/Migrations/20250130021325_UpdateNonNullable.Designer.cs
+++ api/Migrations/20250130021325_UpdateNonNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dress_u_backend.Data;
 
@@ -11,9 +12,11 @@ using dress_u_backend.Data;
 namespace dress_u_backend.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250130021325_UpdateNonNullable")]
+    partial class UpdateNonNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

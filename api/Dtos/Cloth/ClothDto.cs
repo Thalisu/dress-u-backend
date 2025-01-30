2b10e8b4b7f0538539077275594f4b5b52c2d522
Thalisu <thalisondmcc@gmail.com>
2025-01-29 22:20:11 -0300

    refactor: update ClothDto and related models to use non-nullable properties

--- api/Dtos/Cloth/ClothDto.cs
+++ api/Dtos/Cloth/ClothDto.cs
@@ -11,12 +11,11 @@ namespace dress_u_backend.Dtos.Cloth
     {
         public int Id { get; set; }
         public string Title { get; set; } = string.Empty;
-
         public decimal Price { get; set; }
         public float Discount { get; set; }
         public string[] Images { get; set; } = [];
         public int Stock { get; set; }
-        public DescriptionDto? Description { get; set; }
-        public List<CategoryOnlyDto?> Categories { get; set; } = [];
+        public DescriptionDto Description { get; set; } = new();
+        public CategoriesDto Categories { get; set; } = new();
     }
 }

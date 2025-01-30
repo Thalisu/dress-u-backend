63d81fb755b9ccd2f28e14afe3d3985ece3871ea
Thalisu <thalisondmcc@gmail.com>
2025-01-29 22:16:43 -0300

    feat: add CategoriesDto for managing a collection of CategoryDto instances

---
+++ api/Dtos/Category/CategoriesDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dress_u_backend.Dtos.Cloth;
+
+namespace dress_u_backend.Dtos.Category
+{
+    public class CategoriesDto
+    {
+        public IEnumerable<CategoryDto> Categories { get; set; } = [];
+    }
+}
